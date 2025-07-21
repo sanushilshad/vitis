74a38ee2dc70fb8fd975dd35694680c0b088b98c
Sanu Shilshad <sanu.shilshad@acelrtech.com>
2025-07-21 11:55:26 +0000

    perf: :card_file_box: update the department migration file

--- migrations/009_department.sql
+++ migrations/009_department.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS department_account (
   id uuid PRIMARY KEY,
   name TEXT NOT NULL,
-  is_active status DEFAULT 'inactive'::status NOT NULL,
+  is_active status DEFAULT 'active'::status NOT NULL,
   created_by  uuid NOT NULL,
   created_on TIMESTAMPTZ NOT NULL,
   updated_by uuid,
@@ -15,10 +15,11 @@ CREATE TABLE IF NOT EXISTS department_account (
 );
 
 
-CREATE TABLE IF NOT EXISTS department_user_relationship (
+CREATE TABLE IF NOT EXISTS business_user_department_relationship (
   id uuid PRIMARY KEY,
   user_id uuid NOT NULL,
   department_id uuid NOT NULL,
+  business_id uuid NOT NULL,
   role_id uuid NOT NULL,
   verified BOOLEAN NOT NULL DEFAULT false,
   created_on TIMESTAMPTZ NOT NULL,
@@ -28,7 +29,8 @@ CREATE TABLE IF NOT EXISTS department_user_relationship (
 );
 
 
-ALTER TABLE department_user_relationship ADD CONSTRAINT "fk_user_id" FOREIGN KEY ("user_id") REFERENCES user_account ("id") ON DELETE CASCADE;
-ALTER TABLE department_user_relationship ADD CONSTRAINT "fk_department_id" FOREIGN KEY ("department_id") REFERENCES department_account ("id") ON DELETE CASCADE;
-ALTER TABLE department_user_relationship ADD CONSTRAINT "fk_role_id" FOREIGN KEY ("role_id") REFERENCES role ("id") ON DELETE CASCADE;
-ALTER TABLE department_user_relationship ADD CONSTRAINT user_department_role UNIQUE (user_id, department_id, role_id);
+ALTER TABLE business_user_department_relationship ADD CONSTRAINT "fk_user_id" FOREIGN KEY ("user_id") REFERENCES user_account ("id") ON DELETE CASCADE;
+ALTER TABLE business_user_department_relationship ADD CONSTRAINT "fk_department_id" FOREIGN KEY ("department_id") REFERENCES department_account ("id") ON DELETE CASCADE;
+ALTER TABLE business_user_department_relationship ADD CONSTRAINT "fk_business_id" FOREIGN KEY ("business_id") REFERENCES business_account ("id") ON DELETE CASCADE;
+ALTER TABLE business_user_department_relationship ADD CONSTRAINT "fk_role_id" FOREIGN KEY ("role_id") REFERENCES role ("id") ON DELETE CASCADE;
+ALTER TABLE business_user_department_relationship ADD CONSTRAINT user_department_role UNIQUE (user_id, department_id, business_id, role_id);
